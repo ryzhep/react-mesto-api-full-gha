a73a54eadd498938543d36200bb42942e5e9183b
ryzhep <grishina.elena.p@yandex.ru>
2024-01-29 14:29:48 +0000

    Добавила env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const UnauthorizedError = require('../errors/UnauthorizedError');
 const ForbiddenError = require('../errors/UnauthorizedError');
@@ -13,7 +14,10 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : "super_secret_key",
+    );
   } catch (err) {
     throw new UnauthorizedError('Требуется авторизация');
   }
